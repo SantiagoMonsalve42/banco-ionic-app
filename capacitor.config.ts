5f33b24a38dbd81267a173c6eaa47665d9c70777
santiago monsalve <santi.monsalve09@gmail.com>
2021-12-03 20:34:48 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'banco-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
